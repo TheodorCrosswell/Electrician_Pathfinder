2156246115877d3e973ff741ae656869459269ea
Theodor Crosswell <theodor.crosswell@gmail.com>
2026-04-13 18:33:11 -0500

    Attempted fix to enable deployment to Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		tailwindcss(),
 		VitePWA({
 			registerType: 'autoUpdate',
 			manifest: {

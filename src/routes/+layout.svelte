74f064c8932a25a1df979e93217072bb2b97a7df
Theodor Crosswell <theodor.crosswell@gmail.com>
2026-04-17 14:36:58 -0500

    Converted to PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import './layout.css';
+	import { onMount } from 'svelte';
 
+	onMount(() => {
+		if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/sw.js');
+		}
+	});
 	let { children } = $props();
 </script>
 
